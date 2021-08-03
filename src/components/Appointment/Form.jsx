889d6fe501c4cd2150ef672b5b371f2152f3bd15
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-03 19:26:39 -0400

    Stories and code for form component updated to show Create and Update stories. Next step is to add useState.

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -14,21 +14,24 @@ onCancel:Function
 
 export default function Form (props) {
   
-  const {interviewer, onCancel, onSave, interviewers, onChange} = props;
+  const {name, interviewer, onCancel, onSave, interviewers, onChange} = props;
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
+          
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Enter Student Name"
+            placeholder="Enter Student Name" 
+            value = {name}
             /*
               This must be a controlled component
             */
           />
+          
         </form>
         <InterviewerList interviewers={interviewers} value={interviewer} onChange={onChange} />
       </section>
