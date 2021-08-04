494c81f44167a934b12284dcdcd58e490e38b960
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-04 19:08:43 -0400

    Add comments to selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,14 @@
 export function getAppointmentsForDay(state, day) {
   let output = [];
 
+  // Empty array if the days array is empty.
   if(state.days.length === 0){
     return output;
   }
   // Filter out the day objects equal to the passed 'day' variable.
   const filteredDay = state.days.filter(data => data.name === day)
   
+  // Only map if a day was found, otherwise return an empty array.
   if(Object.keys(filteredDay).length) {
     const mappedAppointments = filteredDay[0].appointments.map (el => state.appointments[el])
     output = mappedAppointments;
