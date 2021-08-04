e6fd73f154fd96838bbe7e81f67faf8196e15b9d
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-04 19:22:39 -0400

    Refactor selectors.js code to remove extra code.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,18 +1,15 @@
 export function getAppointmentsForDay(state, day) {
   let output = [];
 
-  // Empty array if the days array is empty.
-  if(state.days.length === 0){
-    return output;
-  }
-  // Filter out the day objects equal to the passed 'day' variable.
-  const filteredDay = state.days.filter(data => data.name === day)
-  
+  // Filter out the day objects equal to the passed 'day' variable - empty if no day is found.
+  const filteredDay = state.days.filter(data => data.name === day);
+
   // Only map if a day was found, otherwise return an empty array.
   if(Object.keys(filteredDay).length) {
-    const mappedAppointments = filteredDay[0].appointments.map (el => state.appointments[el])
-    output = mappedAppointments;
+    // In map, el refers to the appointment number, so appointments[el] accesses the appointment data from state.    
+    output = filteredDay[0].appointments.map (el => state.appointments[el]); 
   }
-
+  
+  // Returns empty if filteredDay is empty.
   return output;
 }
