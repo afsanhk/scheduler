797a3d972fbd3a7f56ae29a74be32219fc7d9754
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-03 21:48:00 -0400

    Remove Fragment import and id deconstruction from index.js in Appointment component directory.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React from "react";
 
 import Header from "./Header";
 import Show from "./Show";
@@ -8,7 +8,7 @@ import "components/Appointment/styles.scss";
 
 export default function Appointment (props) {
   
-  const {id, time, interview} = props;
+  const {time, interview} = props;
 
   return (
     <article className='appointment'>
