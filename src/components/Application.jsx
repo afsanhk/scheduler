ddf3d3be0e991367543f4ee6d3bfddf1afa28676
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-04 19:47:01 -0400

    Refactored code in Application.jsx so that 3 useState hooks were replaced by one, which represented the old 3 states in an object. Used the spread operator to define indivdual 'setState' operations for each obj state.

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -7,74 +7,87 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-},
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm"
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Afsanul Khan",
-      interviewer: {
-        id: 3, 
-        name: "Mildred Nazir", 
-        avatar: "https://i.imgur.com/T2WwVfS.png" 
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Anika Tahsin",
-      interviewer: {
-        id: 4, 
-        name: "Cohana Roy", 
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  },
-  {
-    id: "last",
-    time: "5pm",
-    interview: {
-      student: "Serajum Monira",
-      interviewer: {
-        id: 4, 
-        name: "Cohana Roy", 
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+// },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm"
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Afsanul Khan",
+//       interviewer: {
+//         id: 3, 
+//         name: "Mildred Nazir", 
+//         avatar: "https://i.imgur.com/T2WwVfS.png" 
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Anika Tahsin",
+//       interviewer: {
+//         id: 4, 
+//         name: "Cohana Roy", 
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   },
+//   {
+//     id: "last",
+//     time: "5pm",
+//     interview: {
+//       student: "Serajum Monira",
+//       interviewer: {
+//         id: 4, 
+//         name: "Cohana Roy", 
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   }
+// ];
 
-const appointmentItem = appointments
-  .map(appointment => {
-    return <Appointment key={appointment.id} {...appointment}/>
-  });
+// const appointmentItem = appointments
+//   .map(appointment => {
+//     return <Appointment key={appointment.id} {...appointment}/>
+//   });
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  // Old States
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  // const [appointments, setAppointments] = useState({})
   
+  // Replaced with state object
+  const [state, setState] = useState({
+    day: "Monday", 
+    days: [], 
+    appointments: {}
+  });
+
+  // Basically, only impact the intended state variable without impacting the rest of the 'state' variables.
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
   // Gets Days Data 
   useEffect(() => {
     const url = '/api/days';
@@ -83,7 +96,6 @@ export default function Application(props) {
     .catch(err => console.log(err))
   },[])
 
-  
 
   return (
     <main className="layout">
@@ -95,7 +107,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -104,7 +116,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentItem}
+        {/* {appointmentItem} */}
       </section>
     </main>
   );
