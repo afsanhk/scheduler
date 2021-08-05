7c4959c03b2e11f4104358cc5b6817aa7d2d3201
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-05 16:58:41 -0400

    Add useVisualMode custom hook to Appointment.js and use ti render three views for the appointment component - Empty, Show, Create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,31 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 import "components/Appointment/styles.scss";
 
+import useVisualMode from "hooks/useVisualMode";
+
 export default function Appointment (props) {
   
-  const {time, interview} = props;
+  const { time, interview } = props;
+  // Temporary
+  let interviewers = [];
+
+  // Mode Constants
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(props.interview? SHOW : EMPTY)
 
   return (
     <article className='appointment'>
       <Header time={time}/>
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />} 
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>} 
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} />} 
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()}/>}
     </article>
   )
 }
