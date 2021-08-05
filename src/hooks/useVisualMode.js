1ff4cf42f075f08025e7e8cd3f3efb88a079f2f1
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-05 15:52:49 -0400

    Update useVisualMode.js to use a copy of the state instead of affecting the state directly.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,24 +5,27 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial); // The mode state is what is controlled in useVisualMode
   const [history, setHistory] = useState([initial]); // Tracks history of mode changes
 
+  let historyCopy = [...history]; // Everything works if I just use history. 
   // If you enter a new mode, setState to newMode.
   const transition = (newMode, replace = false) => {
 
     // Replace -- place newMode in place of last mode instead of pushing it after.
     if(replace) { 
-      history.pop(); // Pop out the last mode 
+      historyCopy.pop(); // Pop out the last mode 
     }
 
-    history.push(newMode) // Push new mode to end of history.
-    setMode(history[history.length - 1]); //setMode to the pushed mode in history.
+    historyCopy.push(newMode) // Push new mode to end of history.
+    setHistory(historyCopy); // Since we're using a copy, set the history to the copy state.
+    setMode(historyCopy[historyCopy.length - 1]); //setMode to the pushed mode in history.
   }
 
   // Moves to the previous mode
   const back = () => {
-    if(history.length > 1){ // If there is only one item (i.e. first mode), don't pop.
-      history.pop(); // Otherwise, pop out the last visited mode.
+    if(historyCopy.length > 1){ // If there is only one item (i.e. first mode), don't pop.
+      historyCopy.pop(); // Otherwise, pop out the last visited mode.
     }
-    setMode(history[history.length - 1]); // Visit the new end of array, which was the previous mode.
+    setHistory(historyCopy); // Since we're using a copy, set the history to the copy state.
+    setMode(historyCopy[historyCopy.length - 1]); // Visit the new end of array, which was the previous mode.
   }
 
   return { mode, transition, back };
