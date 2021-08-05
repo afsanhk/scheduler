56caa3645bb9d41cbb3585453c562ccb1d0c5915
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-05 12:31:00 -0400

    Refactored function getInterview in selectors.js --> fixed react crashing.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -21,16 +21,10 @@ export function getInterview(state, interview) {
     return null;
   }
 
-  // Need to go through state and find which appointment contains the interview with interviewer ID equal to the interviewer ID in the interview argument
-  let filteredInterview;
-  let filteredInterviewer;
-  for (const key in state.appointments) {
-    if(state.appointments[key].interview !== null && state.appointments[key].interview.interviewer === interview.interviewer) {
-      filteredInterview = state.appointments[key].interview; // Take the interview with matching ID.
-      filteredInterviewer = interview.interviewer; // Isolate the ID.
-      filteredInterview['interviewer'] = state.interviewers[filteredInterviewer]; // Replace the ID with the corresponding interviewer object from state.
-    }
+  // Simpler way to do than the loop in old commits. -- Helped by LoveJ! 
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
   }
 
-  return filteredInterview;
 }
