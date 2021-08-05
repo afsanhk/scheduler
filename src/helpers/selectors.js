2b53a1ce61f066384dbfa58ca5ac6df8a3a10eec
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-05 17:21:26 -0400

    Add tests and function for getInterviewersForDay function in selectors.js.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -28,3 +28,19 @@ export function getInterview(state, interview) {
   }
 
 }
+
+export function getInterviewersForDay(state,day) {
+  let output = [];
+
+  // Filter out the day objects equal to the passed 'day' variable - empty if no day is found.
+  const filteredDay = state.days.filter(data => data.name === day);
+
+  // Only map if a day was found, otherwise return an empty array.
+  if(Object.keys(filteredDay).length) {
+    // In map, el refers to the appointment number, so appointments[el] accesses the appointment data from state.    
+    output = filteredDay[0].interviewers.map (el => state.interviewers[el]); 
+  }
+  
+  // Returns empty if filteredDay is empty.
+  return output;
+}
