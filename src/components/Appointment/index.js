fccbf6a9c8d205533e7cd7367d0a6e3b3e0619c2
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-06 12:52:01 -0400

    Add confirmation message to delete functionality.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 import "components/Appointment/styles.scss";
 
@@ -20,6 +21,7 @@ export default function Appointment (props) {
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
 
   // Default to SHOW if interview is booked, otherwise empty.
   const { mode, transition, back } = useVisualMode(props.interview? SHOW : EMPTY)
@@ -48,11 +50,13 @@ export default function Appointment (props) {
   return (
     <article className='appointment'>
       <Header time={time}/>
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>} 
-      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={deleteAppt}/>} 
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>} \
+      {/* Ask for a code review for the SHOW props.onDelete code in Show.jsx */}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={() => transition(CONFIRM)}/>} 
       {mode === CREATE && <Form interviewers={interviewers} onSave={save} onCancel={() => back()}/>}
       {mode === SAVING && <Status message={"Saving"} />}
-      {mode === DELETING && <Status message={"Deleting"}/>}    
+      {mode === DELETING && <Status message={"Deleting"}/>}
+      {mode === CONFIRM && <Confirm message={"Are you sure you would like to delete?"} onCancel={() => back()} onConfirm={deleteAppt}/>}    
     </article>
   )
 }
