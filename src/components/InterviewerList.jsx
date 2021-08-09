0d629ce58f87a3132afb89a67afdbbc1a9a5c487
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-08 21:32:07 -0400

    Add prop-types library to project. Ensure InterviewerList prop interviewers is array type.

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   
   const { interviewers, onChange } = props;
 
@@ -24,3 +25,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array
+};
+
+export default InterviewerList;
