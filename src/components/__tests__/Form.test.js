fb548418a7bcb3bcb04b2bda8f73f938e20c7728
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-10 16:47:53 -0400

    Add validation for no interviewer selected in Form.jsx

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -39,7 +39,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
